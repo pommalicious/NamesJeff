7d46f7008d8581d1e1f5709fd7fa995f3db5099f
mrwayneteng <mr.wayneteng@gmail.com>
2015-04-21 15:15:06 -0700

    Splash screen prompts

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $("img.splash").mouseover(function(){
+        $(this).attr('src',"imgs/tatum.jpg");
+        $(this).css("margin" , "auto");
+    });
+
+    $("img.splash").mouseout(function(){
+        $(this).attr('src',"imgs/nametag.jpg");
+    });
+
+
+
+    $("img.splash").click(function(){
+        var names=prompt("What is your Name?");
+        if (names == "jeff" || names == "Jeff"){
+            alert("My name is Jeff!");
+            alert("My name is Jeff!");
+            alert("My name is Jeff!");
+
+        }else {
+            alert(names+" is not your Name");
+            alert("Please enter your actual name");
+        }
+    });
+
+
+});
